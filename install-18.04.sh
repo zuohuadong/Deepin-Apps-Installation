c82d3b89f22f69d64c27828c3b41ea6309de517b
huadong zuo <admin@zuohuadong.cn>
2018-06-12 12:34:46 +0800

    Update install-18.04.sh

--- install-18.04.sh
+++ install-18.04.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo apt-get install lib32z1 lib32ncurses5 p7zip p7zip-full libp11-kit0:i386 libxt6:i386 ocl-icd-libopencl1:i386 odbcinst:i386 odbcinst1debian2:i386 p11-kit-modules:i386 unixodbc:i386 libp11-kit-gnome-keyring:i386
+sudo apt-get install  -y lib32z1 lib32ncurses5 p7zip p7zip-full libp11-kit0:i386 libxt6:i386 ocl-icd-libopencl1:i386 odbcinst:i386 odbcinst1debian2:i386 p11-kit-modules:i386 unixodbc:i386 libp11-kit-gnome-keyring:i386
 sudo dpkg -i libs/*.deb
 sudo dpkg -i deepin-wine/*.deb
 sudo rm -rf /usr/share/doc/libpng16-16/changelog.Debian.gz
